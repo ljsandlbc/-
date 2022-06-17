eeb01ac4790ee79a6fcf328ed7ffb7d10b0d9588
ljsandlbc <96004660+ljsandlbc@users.noreply.github.com>
2022-06-17 14:38:12 +0800

    Update first.py

--- first.py
+++ first.py
@@ -1,11 +1,6 @@
-刘家森=123
-李四=456
-print(刘家森)
-print(李四)
-刘家森,李四=李四,刘家森
-print(刘家森)
-print(李四)
-print("————————")
-print("let's go!")
-print('"every place')
-print('"every let\'s go"')#'被认为是普通字符
+
+print(“hello github”)
+
+#更新一下
+
+print(“hello ljs”)
